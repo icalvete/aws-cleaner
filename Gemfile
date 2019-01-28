de4e552fb35a4fe885d8f027c7edc8a5b32ced5d
Israel Calvete <icalvete@gmail.com>
2019-01-28 13:03:22 +0100

    Some refactor.

--- Gemfile
+++ Gemfile
@@ -1,3 +1,8 @@
 source 'https://rubygems.org'
 
-gemspec
+gem 'aws-sdk-core', '~> 2.0'
+gem 'chef-api', '~> 0.5'
+gem 'hipchat', '~> 1.5'
+gem 'rest-client', '~> 2'
+gem 'slack-poster', '~> 2.2'
+gem 'trollop', '~> 2.1'
---
+++ bin/fake_instances_cleaner.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/ruby
+
+require 'json'
+require 'yaml'
+require 'rest-client'
+require 'trollop'
+require 'timeout'
+require 'pp'
+
+# Main variables
+debug = false
+
+# require our class
+require_relative '../lib/aws-cleaner.rb'
+
+def config(file)
+  YAML.safe_load(File.read(File.expand_path(file)), [Symbol])
+rescue StandardError => e
+  raise "Failed to open config file: #{e}"
+end
+
+# get options
+opts = Trollop.options do
+  opt :config, 'Path to config file', type: :string, default: 'config.yml'
+end
+
+@config = config(opts[:config])
+fake_instances = @config[:fake_instances]
+
+while true
+  fake_instances.each do |fake_instance|
+    if AwsCleaner::Sensu.in_sensu?(fake_instance, @config)
+      puts "#{fake_instance} is in sensu."
+      AwsCleaner::Sensu.remove_from_sensu(fake_instance, @config)
+      puts "#{fake_instance} has been deleted from sensu."
+    else
+    end
+  end
+  sleep(2)
+end
